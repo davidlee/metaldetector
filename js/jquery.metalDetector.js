0ef7e4b1317414632a49e0ad773b0652c51fcbb0
davidlee <david@davelee.com.au>
2010-05-28 16:35:19 +1000

    winning

--- js/jquery.metalDetector.js
+++ js/jquery.metalDetector.js
@@ -1,5 +1,7 @@
+// 
+
 (function($) {
-  var collectionCache, input, collection, options;
+  var input, collection, options;
 
   var defaults = {
     // collection: '#searchable',
@@ -7,31 +9,32 @@
     text: function(el) { return $(el).text(); }
   };
   
-  // sort method
-  function byScore(a,b) {
-    return $(a).data('liquidmetal.score') > $(b).data('liquidmetal.score');
-  }
+  function sortByScore(a, b) {
+    return $(b).data('md.score') - $(a).data('md.score')
+  };
   
-  function doUpdate() {
+  function doSearch() {
     var query, elements, el, string, score;
     query    = input.val();
     elements = $(options.target);
-    collection.html($.each(elements, function() {
-      el     = $(this);
-      string = options.text(el);
-      score  = LiquidMetal.score(string, query);
-      el.data('liquidmetal.score', score);            
-      if (score > (options.minimumScore || 0)) {
-        el.show();
-      } else {
-        el.hide();
-      }      
-    }).sort(byScore));
+    collection.html(
+      $.each(elements, function() {
+        el     = $(this);
+        string = options.text(el);
+        score  = LiquidMetal.score(string, query);
+        el.data('md.score', score);            
+        if (score > (options.minimumScore || 0)) {
+          el.show();
+        } else {
+          el.hide();
+        }      
+      }).sort(sortByScore)
+    );
   };
 
   // restore the collection to the way it was in collectionCache
   function doReset() {
-    collection.html(collection.data('elementCache'));
+    collection.html(collection.data('md.cache'));
   };
   
   function assertHasProperties (obj, propertyList) {
@@ -40,20 +43,35 @@
     });
   };
 
-  $.fn.sort = function() { return this.pushStack([].sort.apply(this, arguments), []); };
+  // this is only required if using a legacy version of jQuery
+  // $.fn.sort = function() { return this.pushStack([].sort.apply(this, arguments), []); };
   
   $.fn.metalDetector = function(userOptions) {
     input      = $(this);
-    options    = $.extend(defaults, userOptions);    
+    options    = $.extend( (input.data('md.options') || defaults), userOptions);
+    // options are stored to be used as defaults on subsequent invocations
+    // i.e., options do not need to be passed in again when refreshing (see below)
+    input.data('md.options', options);
     // validate options
     assertHasProperties(options, ['collection', 'target', 'text']);
-    // store the elements in their original position
+    // store the elements in their original position and state
+    // if the elements are ever updated, eg. via an ajax call, 
+    // just call $(element).metalDetector() again.
     collection = $(options.collection);
-    collection.data('elementCache', collection.html());
-    // collectionCache = $(options.collection).html();
-    // add the event handler
-    $(this).keyup(function(e){ input.val() == '' ? doReset() : doUpdate(); });
-    // TODO export methods for manipulating the collection cache
+    collection.data('md.cache', collection.html());
+    
+    $(this).unbind('keyup');
+    $(this).keyup(function(e){ input.val() == '' ? doReset() : doSearch(); });
+    $(this).trigger('keyup');
+    return this;
   };
 
 })(jQuery);
+
+// e.g.
+//
+// $('#search').metalDetector({
+//   collection: '#searchable',
+//   target: '#searchable tr'
+// });
+// 
